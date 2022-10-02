4769e4e575ba87fef66250d7dc1dba9c4b59a0e8
muratcanaltun <66438521+muratcanaltun@users.noreply.github.com>
2022-10-02 22:23:37 +0300

    initial commit

---
+++ bullseyeindexservice/index_calculator_client.cc
@@ -0,0 +1,71 @@
+#include <grpcpp/grpcpp.h>
+#include <string>
+#include "bullseyeindexservice.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using bullseyeindexservice::IndexReply;
+using bullseyeindexservice::IndexRequest;
+using bullseyeindexservice::IndexCalc;
+
+class IndexCalcClient {
+public:
+	IndexCalcClient(std::shared_ptr<Channel> channel) : stub_(IndexCalc::NewStub(channel)) {}
+
+	std::string sendRequest(int index_id)
+	{
+		IndexRequest request;
+		request.set_index_id(index_id);
+
+		IndexReply reply;
+		ClientContext context;
+
+		Status status = stub_->sendRequest(&context, request, &reply);
+
+		if (status.ok())
+		{
+			return std::to_string(reply.index_value());
+		}
+		else
+		{
+			std::cout << "Error code " << status.error_code() << ": " << status.error_message() << std::endl;
+			return "RPC Failed";
+		}
+
+		// if statement
+	}
+private:
+	std::unique_ptr<IndexCalc::Stub> stub_;
+};
+
+void RunClient(int index_id) 
+{
+	// init IP adress, response str and client
+	std::string target_address("127.0.0.1:50051");
+	std::string response;
+	IndexCalcClient client(grpc::CreateChannel(target_address, grpc::InsecureChannelCredentials()));
+
+	// get timestamps and response
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	response = client.sendRequest(index_id);
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+	// get the difference between the two timestamps, turn to duration
+	std::chrono::duration<double, std::milli> time_spent = t2 - t1;
+
+	// print results
+	std::cout << "Calculated in " << time_spent.count() << " ms." << std::endl;
+	std::cout << "Fetched for ID: " << index_id << std::endl;
+	std::cout << "Received: " << response << std::endl;
+}
+
+int main()
+{
+	int index_id;
+	std::cout << "Please enter the ID of the index: " << std::endl;
+	std::cin >> index_id;
+
+	RunClient(index_id);
+}
---
+++ bullseyeindexservice/index_calculator_server.cc
@@ -0,0 +1,63 @@
+#include <grpcpp/grpcpp.h>
+#include <string>
+#include "bullseyeindexservice.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using bullseyeindexservice::IndexReply;
+using bullseyeindexservice::IndexRequest;
+using bullseyeindexservice::IndexCalc;
+
+class IndexCalcServiceImplementation final : public IndexCalc::Service
+{
+	Status sendRequest(ServerContext* context, const IndexRequest* request, IndexReply* reply) override
+	{
+		int index_id = request->index_id();
+
+		int n = 1;
+		int price = 0;
+
+		switch (index_id)
+		{
+		case 1:
+			n = 10;
+			break;
+		case 2:
+			n = 100;
+			break;
+		default:
+			n = 10000;
+			break;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			price += i;
+		}
+
+		reply->set_index_value(price);
+		return Status::OK;
+	}
+};
+
+void RunServer()
+{
+	std::string server_address("127.0.0.1:50051");
+	IndexCalcServiceImplementation service;
+
+	ServerBuilder builder;
+	builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+	builder.RegisterService(&service);
+
+	std::unique_ptr<Server> server(builder.BuildAndStart());
+	std::cout << "Bullseye Index Service listening on: " << server_address << std::endl;
+
+	server->Wait();
+}
+
+int main()
+{
+	RunServer();
+}
