eb0c8e6658f54adb49b9dc766bbe41f714be8c1a
muratcanaltun <murat.altun@ozu.edu.tr>
2023-05-20 00:53:06 +0300

    added local stock data

--- bullseyeindexservice/index_calculator_server.cc
+++ bullseyeindexservice/index_calculator_server.cc
@@ -3,9 +3,6 @@
 #include <sstream>
 
 #include "bullseyeindexservice.grpc.pb.h"
-#include <curl/curl.h>
-#include <rapidjson/document.h>
-#include <rapidjson/istreamwrapper.h>
 #include <bsoncxx/json.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/stdx.hpp>
@@ -15,14 +12,14 @@
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/builder/stream/array.hpp>
 
-#define CURL_MAX_WRITE_SIZE 32768
-
 using bsoncxx::builder::stream::close_array;
 using bsoncxx::builder::stream::close_document;
 using bsoncxx::builder::stream::document;
 using bsoncxx::builder::stream::finalize;
 using bsoncxx::builder::stream::open_array;
 using bsoncxx::builder::stream::open_document;
+using bsoncxx::builder::basic::make_document;
+using bsoncxx::builder::basic::kvp;
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -39,6 +36,9 @@ mongocxx::client client(uri);
 mongocxx::database db = client["Index"];
 mongocxx::collection coll = db["Index_Values"];
 
+mongocxx::database stockDB = client["Stock"];
+mongocxx::collection stock_coll = stockDB["Stocks"];
+
 size_t write_to_string(void* contents, size_t size, size_t nmemb, std::string* s);
 
 class IndexCalcServiceImplementation final : public IndexCalc::Service
@@ -46,10 +46,8 @@ class IndexCalcServiceImplementation final : public IndexCalc::Service
 
 	Status sendRequest(ServerContext* context, const IndexRequest* request, IndexReply* reply) override
 	{
-		CURL* curl;
-		CURLcode res;
-		std::string link = "https://query1.finance.yahoo.com/v7/finance/quote?symbols=";
 		std::string index_name = request->index_id();
+		std::map<std::string, std::pair<double, int>> stocks;
 
 		std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 		bsoncxx::stdx::optional<bsoncxx::document::value> index_result = coll.find_one(document{} << "Name" << index_name << finalize);
@@ -57,58 +55,49 @@ class IndexCalcServiceImplementation final : public IndexCalc::Service
 
 		std::string calculation_type = index_view["Type"].get_utf8().value.to_string();
 		double index_divisor = index_view["Divisor"].get_double();
-		link = link.append(index_view["Symbols"].get_utf8().value.to_string());
 
-		curl = curl_easy_init();
-		std::string stringify;
+		std::istringstream iss(index_view["Symbols"].get_utf8().value.to_string());
 
-		if (curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, link.c_str());
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &stringify);
+		do {
+			std::string stock_name;
+			iss >> stock_name;
 
-			res = curl_easy_perform(curl);
+			if (stock_name.empty()) {
+				break;
+			}
 
-			curl_easy_cleanup(curl);
-		}
+			bsoncxx::stdx::optional<bsoncxx::document::value> stock = stock_coll.find_one(make_document(kvp("stock_symbol", stock_name)));
+			
+			auto stock_view = stock->view();
+
+			stocks[stock_name] = std::make_pair(stock_view["stock_price"].get_double(), stock_view["stock_volume"].get_int32().value);
+
+		} while (iss);
 
 		std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 		std::chrono::duration<double, std::milli> time_spent = t2 - t1;
 		std::cout << "Fetched stock data for " << index_name << " in " << time_spent.count() << " ms." << std::endl;
 
-		rapidjson::Document doc;
-		std::stringstream ss(stringify);
-		rapidjson::IStreamWrapper isw(ss);
-
-		doc.ParseStream<rapidjson::kParseStopWhenDoneFlag>(isw);
-
-		assert(doc.IsObject());
-
-		rapidjson::Value& quoteResponse = doc["quoteResponse"];
-		rapidjson::Value& result = quoteResponse["result"];
-
-		assert(result.IsArray());
-
 		double price = 0;
 
-		for (auto const& p : quoteResponse["result"].GetArray()) {
+		for (auto const& p : stocks) {
 			if (calculation_type.compare("Price Weighted") == 0) {
-				price += p["regularMarketPrice"].GetDouble();
+				price += p.second.first;
 			}
 			else if (calculation_type.compare("Capitalization Weighted") == 0) {
-				price += p["regularMarketPrice"].GetDouble() * p["sharesOutstanding"].GetDouble();
+				price += p.second.first * p.second.second;
 			}
 			else if (calculation_type.compare("Equal Weighted") == 0) {
-				price += p["regularMarketPrice"].GetDouble() * (1.0 / quoteResponse["result"].GetArray().Size());
+				price += p.second.first * (1.0 / stocks.size());
 			}
 			else if (calculation_type.compare("Capped") == 0) {
-				double shares = p["sharesOutstanding"].GetDouble();
+				double shares = p.second.second;
 
-				if (index_view["Limit"].get_double() < p["sharesOutstanding"].GetDouble()) {
+				if (index_view["Limit"].get_double() < p.second.second) {
 					shares = index_view["Limit"].get_double();
 				}
 
-				price += p["regularMarketPrice"].GetDouble() * shares;
+				price += p.second.first * shares;
 			}
 		}
 
