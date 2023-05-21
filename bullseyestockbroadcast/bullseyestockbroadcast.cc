651dca2fada4d7135a8a90f724798f62da1569c6
muratcanaltun <murat.altun@ozu.edu.tr>
2023-05-21 15:58:17 +0300

    added stock hist value broadcasting

--- bullseyestockbroadcast/bullseyestockbroadcast.cc
+++ bullseyestockbroadcast/bullseyestockbroadcast.cc
@@ -61,17 +61,59 @@ void handle_get(web::http::http_request request)
     request.reply(response);
 }
 
+void handle_hist_get(web::http::http_request request)
+{
+    mongocxx::uri uri("mongodb://localhost:27017");
+    mongocxx::client client(uri);
+    mongocxx::database db = client["Stock"];
+    mongocxx::collection coll = db["Stock_Hist"];
+
+    web::http::http_response response(web::http::status_codes::OK);
+    response.headers().add(utility::conversions::to_string_t("Access-Control-Allow-Origin"), utility::conversions::to_string_t("*"));
+    response.headers().add(utility::conversions::to_string_t("Access-Control-Allow-Methods"), utility::conversions::to_string_t("GET"));
+    response.headers().add(utility::conversions::to_string_t("Access-Control-Allow-Headers"), utility::conversions::to_string_t("access-control-allow-headers,access-control-allow-methods,access-control-allow-origin"));
+
+    auto path = web::http::uri::split_path(web::http::uri::decode(request.relative_uri().path()));
+    std::string splitted(utility::conversions::to_utf8string(path[0]));
+
+    std::cout << "Handling GET " + splitted << std::endl;
+    auto answer = web::json::value::object();
+
+    mongocxx::cursor cursor = coll.find(document{} << "stock_symbol" << splitted << finalize);
+
+    std::vector<web::json::value> values;
+    for (auto doc : cursor) {
+        web::json::value object = web::json::value::object();
+
+        object[utility::conversions::to_string_t("stock_symbol")] = web::json::value::string(utility::conversions::to_string_t(doc["stock_symbol"].get_utf8().value.to_string()));
+        object[utility::conversions::to_string_t("stock_price")] = web::json::value::number((doc["stock_price"].get_double().value));
+        object[utility::conversions::to_string_t("timestamp")] = web::json::value::number((doc["timestamp"].get_date().value.count()));
+
+        values.push_back(object);
+    }
+    answer[utility::conversions::to_string_t("response")] = web::json::value::array(values);
+
+    response.set_body(answer);
+    request.reply(response);
+}
+
 int main()
 {
     web::http::experimental::listener::http_listener listener(utility::conversions::to_string_t("http://localhost:9991/Stocks/"));
+    web::http::experimental::listener::http_listener hist_listener(utility::conversions::to_string_t("http://localhost:9991/Stocks_Hist/"));
 
     listener.support(web::http::methods::GET, handle_get);
+    hist_listener.support(web::http::methods::GET, handle_hist_get);
 
     try
     {
         listener
             .open()
             .then([&listener]() {std::cout << "Started to Listen" << std::endl; })
+            .wait();
+        hist_listener
+            .open()
+            .then([&hist_listener]() {std::cout << "Started to Listen HIST" << std::endl; })
             .wait();
 
         while (true);
